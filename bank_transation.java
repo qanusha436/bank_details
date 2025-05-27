c87a05d5b7e4e6b435c07b173c08618445184961
qanusha436 <amoghajoshi2005@gmail.com>
2025-05-27 11:33:55 +0530

    saved

---
+++ bank_transation.java
@@ -0,0 +1,31 @@
+package bank_details;
+
+public class bank_transation {
+    String accountname;
+    String accountholder;
+    String accounttype;
+    int accountnumber;
+    double balance;
+    
+
+    public bank_transation(String accountname,String accountholder,String accounttype,int accountnumber,double balance){
+        this.accountname=accountname;
+        this.accountholder=accountholder;
+        this.accounttype=accounttype;
+        this.accountnumber=accountnumber;
+        this.balance=balance;
+    }
+    public void display(){
+        System.out.println("account name is:"+accountname);
+        System.out.println("account holder name:"+accountholder);
+        System.out.println("account type:"+accounttype);
+        System.out.println("account number:"+accountnumber);
+        System.out.println("account balance:"+balance);
+    }
+    public static void main(String[] args) {
+        bank_transation transation=new bank_transation("canara","kavya","saving",3588,85858.85);
+        transation.display();
+
+}
+
+}
